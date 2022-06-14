03b30373cf49dfe9e2de5116ecd850f0a24a9c99
Liam Potter <liam@liampotter.co.uk>
2022-06-14 03:24:44 +0100

    remove docs from main

--- docs-src/app/controllers/application.js
+++
@@ -1,5 +0,0 @@
-import Controller from '@ember/controller';
-
-export default class ApplicationController extends Controller {
-  year = new Date().getFullYear();
-}
--- docs-src/app/routes/application.js
+++
@@ -1,5 +0,0 @@
-import Route from '@ember/routing/route';
-import 'ember-fast-marquee/marquee.css';
-import './styles.css';
-
-export default class ApplicationRoute extends Route {}
